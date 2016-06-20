3f4b7668b9340fec11b96380563cbe041bac0862
serbel11 <sergei.belashov@gmail.com>
2016-06-21 01:19:53 +0300

    Убрал quick_sort, записываю слова в массив с помощью хэш-функции

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Sorting_Searching/156 - Ananagrams (2).cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Sorting_Searching/156 - Ananagrams (2).cpp
@@ -11,6 +11,7 @@ struct word
 {
 	string key;
 	int count;
+	count(int n) {};
 };
 
 string convert(string s) {
@@ -22,49 +23,23 @@ string convert(string s) {
 int sumSymb(string s) {
 	int sum = 0;
 	for (int i = 0; i < s.length(); i++)
-		sum += s[i];
+		sum += (s[i] - 97)*(s[i] - 97);
 	return sum;
-}
-
-void quick_sort(string &s, int beginPos, int endPos)
-{
-
-	int i = beginPos, j = endPos;
-	char compareWith = s[(endPos - beginPos) / 2];
-	while (i <= j){
-		while (s[i] < compareWith) 
-			i++;
-		while (s[j] > compareWith) 
-			j--;
-		if (i <= j){
-			char buf = s[i];
-			s[i] = s[j];
-			s[j] = buf;
-		}
-		
-		
-	}
-	if (abs(beginPos - i) >= 2)
-		quick_sort(s, beginPos, i);
-	if (abs(endPos - j) >= 2)
-		quick_sort(s, j, endPos);
-	
-	
 }
 
 int main(){
 	string str;
-	word words[501];
-	/*while (cin >> str){
+	string word[13521];
+	int count[13521] = { 0 };
+	int mapIndex[10000] = { 0 };
+	int i = 0;
+	while (cin >> str){
 		if (str == "#")
 			break;
-		if (words[sumSymb(str)].count == 0){
-
-		}
-	}*/
-	cin >> str;
-	quick_sort(str, 0, str.length() - 1);
-	cout << str;
+		word[sumSymb(str)] = str;
+		count[sumSymb(str)]++;
+		mapIndex[i] = sumSymb(str);
+	}
 
 	return 0;
 }
