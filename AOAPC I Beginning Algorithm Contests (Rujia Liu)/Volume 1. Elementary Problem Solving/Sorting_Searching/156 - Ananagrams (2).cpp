36d83f61dfd43aa910f1d69172cb9e1b6ad8c798
serbel11 <sergei.belashov@gmail.com>
2016-06-21 04:04:58 +0300

    Пытаюсь сделать и сортировку символов, и строк через дерево, через хэш-функцию не получилось

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Sorting_Searching/156 - Ananagrams (2).cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Sorting_Searching/156 - Ananagrams (2).cpp
@@ -7,24 +7,40 @@
 
 using namespace std;
 
-string convert(string s) {
+string toLower(string s) {
 	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-	sort(s.begin(), s.end());
 	return s;
 }
 
-struct tree
+struct word
 {
 	string data;
-	tree* left;
-	tree* right;
+	word* left;
+	word* right;
+	int count;
+};
+
+struct letter
+{
+	char l;
+	letter* left;
+	letter* right;
 };
 
 int sumSymb(string s) {
 	int sum = 0;
-	for (unsigned int i = 0; i < s.length(); i++)
+	for (unsigned int i = 0; i < s.length(); i++){
 		sum += (s[i] - 97)*(s[i] - 97);
+	}
 	return sum;
+}
+
+float qSymb(string s) {
+	float q = 0;
+	for (unsigned int i = 0; i < s.length(); i++){
+		q *= (s[i] - 97);
+	}
+	return q;
 }
 
 int strCompare(string s1, string s2) {
@@ -42,20 +58,62 @@ int strCompare(string s1, string s2) {
 	return s1.length() - s2.length();
 }
 
-int Insert(tree* &begin, string s){
-	tree *actual;
+int Insert(word* &begin, string s){
+	word *actual;
 	if (begin == NULL){
-		tree *newElement = new tree;
+		word *newElement = new word;
 		begin = newElement;
 		begin->data = s;
+		begin->left = begin->right = NULL;
+		begin->count = 1;
+	}
+	else{
+		actual = begin;
+		while (true){
+			word* buf = actual;
+			int lr = 0;
+			if (strCompare(s, actual->data) < 0){
+				actual = actual->right;
+				lr = 1;
+			}
+			else if (strCompare(s, actual->data) > 0)
+				actual = actual->left;
+			else{
+				actual->count++;
+				break;
+			}
+
+			if (actual == NULL){
+				word *newElement = new word;
+				actual = newElement;
+				actual->data = s;
+				actual->left = actual->right = NULL;
+				if (lr == 0)
+					buf->left = actual;
+				else
+					buf->right = actual;
+				actual->count = 1;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+int InsertLetter(letter* &begin, char c){
+	letter *actual;
+	if (begin == NULL){
+		letter *newElement = new letter;
+		begin = newElement;
+		begin->l = c;
 		begin->left = begin->right = NULL;
 	}
 	else{
 		actual = begin;
 		while (true){
-			tree* buf = actual;
+			letter* buf = actual;
 			int flag = 0;
-			if (strCompare(s, actual->data) < 0){
+			if (c < actual->l){
 				actual = actual->right;
 				flag = 1;
 			}
@@ -63,9 +121,9 @@ int Insert(tree* &begin, string s){
 				actual = actual->left;
 
 			if (actual == NULL){
-				tree *newElement = new tree;
+				letter *newElement = new letter;
 				actual = newElement;
-				actual->data = s;
+				actual->l = c;
 				actual->left = actual->right = NULL;
 				if (flag == 0)
 					buf->left = actual;
@@ -78,7 +136,7 @@ int Insert(tree* &begin, string s){
 	return 0;
 }
 
-void Print(tree* begin){
+void Print(word* begin){
 	if (begin != NULL)
 	{
 		Print(begin->left);
@@ -87,27 +145,28 @@ void Print(tree* begin){
 	}
 }
 
-int main(){
-	string str, word[13521];
-	short int count[13521] = { 0 };
-	int mapIndex[10000], i = 0;
-
-	while (cin >> str){
-		if (str == "#")
-			break;
-		int index = sumSymb(convert(str));
-		word[index] = str;
-		count[index]++;
-		mapIndex[i] = index;
-		i++;
+string sort_word(letter* begin, string &s){
+	
+	if (begin != NULL)
+	{
+		sort_word(begin->left, s);
+		s += begin->l;
+		sort_word(begin->right, s);
 	}
+	return s;
+}
 
-	tree* begin = NULL;
-	for (int j = 0; j < i; j++)
-		if (count[mapIndex[j]] == 1)
-			Insert(begin, word[mapIndex[j]]);
+int main(){
+	string str;
+	int i;
 
-	Print(begin);
+
+	letter* begin = NULL;
+	string buf = "wertyuawgn";
+	for (int i = 0; i < buf.length(); i++)
+		InsertLetter(begin, buf[i]);
+	
+	cout << sort_word(begin, buf);
 
 	return 0;
 }
