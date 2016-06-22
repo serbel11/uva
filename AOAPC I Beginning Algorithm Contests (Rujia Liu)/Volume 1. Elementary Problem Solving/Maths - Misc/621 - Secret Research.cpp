48b991774166095619d3f02542a09554a76c27e8
serbel11 <sergei.belashov@gmail.com>
2016-06-22 22:15:10 +0300

    621

---
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Maths - Misc/621 - Secret Research.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, q;
+	cin >> n;
+	while (n--){
+		cin >> q;
+		if (q == 1 || q == 4 || q = 78){
+			cout << "+" << endl;
+			break;
+		}
+	}
+
+
+	return 0;
+}
