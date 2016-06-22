23d65c0a0cdd8d234154aaeef757476f307a3629
serbel11 <sergei.belashov@gmail.com>
2016-06-22 21:03:21 +0300

    Дописал, исправляю ошибки

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Maths - Misc/10161 - Ant on a Chessboard.cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/Maths - Misc/10161 - Ant on a Chessboard.cpp
@@ -8,25 +8,35 @@ using namespace std;
 int main() {
 	int n, x, y;
 	while (cin >> n){
+
 		if (n == 0)
 			break;
+
 		int q = 1;
-		for (int i; i*i < n; i++){
+		for (int i = 1; i*i < n; i++){
 			q = i;
 		}
 		int mid = q * q - (q - 1), 
 			min = q * q - 2 * (q - 1);
 
-		if (n == mid)
+		if (n == mid){
 			x = y = q;
-		if (q % 2 == 0){
-			
-		}
-		else{
-			
+			continue;
 		}
 
+		if (n < mid){
+			y = q;
+			x = n - min + 1;
+		}
+		else{
+			x = q;
+			y = q*q - n + 1;
+		}
 		
+		if (q % 2 != 0)
+			swap(x, y);
+
+		printf("%d %d\n", x, y);
 
 
 	}
